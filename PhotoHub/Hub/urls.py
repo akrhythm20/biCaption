b75eda31b7fb252740de0f0588eab6558c18b2e7
Rohit Jha <rohitj.ug19.cs@nitp.ac.in>
2022-01-28 18:55:32 +0530

    Added initial files along with DjangoAllAuth integration

---
+++ PhotoHub/Hub/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from Hub import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('profile', views.profile, name='profile'),
+    path('login', views.loginUser, name='login'),
+    path('logout', views.logoutUser, name='logout'),
+    path('register', views.register, name='register'),
+]
